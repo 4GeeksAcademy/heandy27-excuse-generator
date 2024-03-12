1c387fd1e3249d66956f6076af8aa02b97ecd9da
Heandy27 <122308561+Heandy27@users.noreply.github.com>
2024-03-12 02:37:32 +0000

    [Excuse generator Index.html y App.js] [Index.html: Agregué en el index un párrafo con un id excuse.] [App.js: creación de la función y asigandole al párrafo el texto.]

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function excuseGenerator() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let numeroAletorioWho = Math.floor(Math.random() * who.length);
+    let numeroAletorioAction = Math.floor(Math.random() * action.length);
+    let numeroAletorioWhat = Math.floor(Math.random() * what.length);
+    let numeroAletorioWhen = Math.floor(Math.random() * when.length);
+
+    return `${who[numeroAletorioWho]} ${action[numeroAletorioAction]} ${what[numeroAletorioWhat]} ${when[numeroAletorioWhen]}`;
+  }
+  let result = excuseGenerator();
+  console.log(result);
+
+  let excuseParagraph = document.getElementById("excuse");
+  console.log({ excuseParagraph });
+  excuseParagraph.innerText = result;
 };
